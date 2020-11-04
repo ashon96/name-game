b3b2c324e49aef095264981a9d76c6a936bf2b28
Andrew Shon <59924169+ashon96@users.noreply.github.com>
2020-11-04 15:14:17 -0500

    Connect to the given API with axios

---
+++ src/components/Page/GamePage/helpers.ts
@@ -0,0 +1,16 @@
+import Axios from "axios";
+
+export const fetchData = async () => {
+  const url = "https://willowtreeapps.com/api/v1.0/profiles";
+
+  await Axios.get(url)
+    .then(function (response) {
+      console.log("do we get here");
+
+      console.log(response.data);
+      return response.data;
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+};
--- src/components/Page/GamePage/index.tsx
+++ src/components/Page/GamePage/index.tsx
@@ -1,16 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useHistory } from "react-router-dom";
 import back from "./Arrow.svg";
 import title from "./Title.svg";
 
 import "./styles.css";
+import { fetchData } from "./helpers";
 
 const GamePage: React.FC = () => {
   const history = useHistory();
 
   // TODO: Have a useEffect here to populate useState with data
+  useEffect(() => {
+    // TODO
+    fetchData();
+    // console.log(jsonObject);
+  }, []);
   // have a counter of number of correct and incorrect guesses
+  const [data, setData] = React.useState<any[]>([]); // TODO: set the type constraint
+  const [correctGuesses, setCorrectGuesses] = React.useState<number>(0);
   // have a useState for clicked boolean. If clicked, then, reset
+  const [hasBeenClicked, setHasBeenClicked] = React.useState<boolean>(false);
 
   // add hover state to arrow
   return (
